50ca3dd5f6661469e739d3e5e3a24b7d21ede104
Chris Dailey <nitz@users.noreply.github.com>
2021-11-04 12:11:55 -0400

    Forgot EGL is static, these don't work well as extension methods :)

---
+++ Editor/EditorGUILayoutEx.cs
@@ -0,0 +1,80 @@
+using cmdwtf.UnityTools;
+
+using UnityEditor;
+
+using UnityEngine;
+
+namespace cmdwtf.UnityTools.Editor
+{
+	public static class EditorGUILayoutEx
+	{
+		/// <inheritdoc cref="EditorGUI.DropdownButton(UnityEngine.Rect,UnityEngine.GUIContent,UnityEngine.FocusType)"/>
+		public static bool DropdownButtonLabeled(string label,
+												 string dropdownContent,
+												 FocusType dropdownFocusType,
+												 GUIStyle labelStyle = null,
+												 GUIStyle dropdownStyle = null
+		)
+			=> DropdownButtonLabeled(label.ToGUIContent(), dropdownContent.ToGUIContent(), dropdownFocusType, labelStyle, dropdownStyle);
+
+		/// <inheritdoc cref="EditorGUI.DropdownButton(UnityEngine.Rect,UnityEngine.GUIContent,UnityEngine.FocusType)"/>
+		public static bool DropdownButtonLabeled(string label,
+												 GUIContent dropdownContent,
+												 FocusType dropdownFocusType,
+												 GUIStyle labelStyle = null,
+												 GUIStyle dropdownStyle = null
+		)
+			=> DropdownButtonLabeled(label.ToGUIContent(), dropdownContent, dropdownFocusType, labelStyle, dropdownStyle);
+
+		/// <inheritdoc cref="EditorGUI.DropdownButton(UnityEngine.Rect,UnityEngine.GUIContent,UnityEngine.FocusType)"/>
+		public static bool DropdownButtonLabeled(GUIContent label,
+												 string dropdownContent,
+												 FocusType dropdownFocusType,
+												 GUIStyle labelStyle = null,
+												 GUIStyle dropdownStyle = null
+		)
+			=> DropdownButtonLabeled(label, dropdownContent.ToGUIContent(), dropdownFocusType, labelStyle, dropdownStyle);
+
+		/// <inheritdoc cref="EditorGUI.DropdownButton(UnityEngine.Rect,UnityEngine.GUIContent,UnityEngine.FocusType)"/>
+		public static bool DropdownButtonLabeled(GUIContent label,
+												 GUIContent dropdownContent,
+												 FocusType dropdownFocusType,
+												 GUIStyle labelStyle = null,
+												 GUIStyle dropdownStyle = null
+		)
+		{
+			const float fortyPercent = 0.4f;
+			const float sixtyPercent = 0.6f;
+
+			// get a 'whole' rect for our control.
+			Rect rect = EditorGUILayout.GetControlRect();
+
+			// duplicate the main rect, and shrink it
+			// to 40% for the label.
+			var labelRect = new Rect(rect);
+			labelRect.width *= fortyPercent;
+
+			// duplicate the main rect, and shrink it
+			// to 60% for the dropdown button, and
+			// scoot it over the width of the label.
+			var dropdownRect = new Rect(rect);
+			dropdownRect.width *= sixtyPercent;
+			dropdownRect.x += labelRect.width;
+
+			// draw the label
+			if (labelStyle != null)
+			{
+				EditorGUI.LabelField(labelRect, label, labelStyle);
+			}
+			else
+			{
+				EditorGUI.LabelField(labelRect, label);
+			}
+
+			// draw the dropdown, and return the user clicked value from it.
+			return dropdownStyle != null
+					   ? EditorGUI.DropdownButton(dropdownRect, dropdownContent, dropdownFocusType, dropdownStyle)
+					   : EditorGUI.DropdownButton(dropdownRect, dropdownContent, dropdownFocusType);
+		}
+	}
+}
