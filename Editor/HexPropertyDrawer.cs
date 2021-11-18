377a91f5c788f4b3c0dcdaf7be5a5a41708f3b11
Chris Dailey <nitz@users.noreply.github.com>
2021-11-18 08:46:58 -0500

    Added `[Hex]` and drawer.

---
+++ Editor/HexPropertyDrawer.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Globalization;
+
+using cmdwtf.UnityTools.Attributes;
+
+using UnityEditor;
+
+using UnityEngine;
+
+namespace cmdwtf.UnityTools.Editor
+{
+	[CustomPropertyDrawer(typeof(HexAttribute))]
+	public class HexAttributeEditor : PropertyDrawer
+	{
+		public override void OnGUI(Rect rect, SerializedProperty property, GUIContent label)
+		{
+			var hexAttribute = (HexAttribute)attribute;
+
+			EditorGUI.BeginProperty(rect, label, property);
+
+			switch (property.propertyType)
+			{
+				case SerializedPropertyType.Integer:
+					DrawPropertyForInt(rect, property, label, hexAttribute);
+					break;
+				default:
+					EditorGUI.LabelField(rect, $"{label.text}: Unsupported type - {property.propertyType}");
+					break;
+			}
+
+			EditorGUI.EndProperty();
+		}
+
+		private static void DrawPropertyForInt(Rect rect, SerializedProperty property, GUIContent label, HexAttribute hexAttribute)
+		{
+			int width = hexAttribute.MinimumDisplayWidth;
+
+			if (width < 0)
+			{
+				width = hexAttribute.GetDefaultWidthForType(property.type);
+			}
+
+			string stringValue = width <= 0
+									 ? $"0x{property.longValue:X}"
+									 : "0x" + property.longValue.ToString($"X{width}");
+
+			stringValue = EditorGUI.TextField(rect, label, stringValue).ToLower();
+
+			if (stringValue.StartsWith("0x"))
+			{
+				// strip the 0x from the beginning, then parse as hex.
+				string no0X = stringValue.Remove(0, 2);
+				if (long.TryParse(no0X, NumberStyles.HexNumber, CultureInfo.CurrentCulture, out long resultHex))
+				{
+					property.longValue = resultHex;
+				}
+			}
+			else if (long.TryParse(stringValue, NumberStyles.Any, CultureInfo.CurrentCulture, out long resultAny))
+			{
+				// no 0x, parse the number as decimal
+				property.longValue = resultAny;
+			}
+			else if (string.IsNullOrWhiteSpace(stringValue))
+			{
+				// no value, default to zero.
+				property.longValue = 0;
+			}
+		}
+	}
+}
