807e07d2a8a3e0817f2942de4935091d5836d024
Chris Dailey <nitz@users.noreply.github.com>
2021-12-01 08:47:35 -0500

    Added ColorTools to convert bytes to `UnityEngine.Color`.

--- Runtime/ColorTools.cs
+++ Runtime/ColorTools.cs
@@ -20,5 +20,22 @@ namespace cmdwtf.UnityTools
 
 			return Color.Lerp(two, one, lerpVal);
 		}
+
+		public static Color FromBytes(byte r, byte g, byte b, byte a = 255)
+		{
+			byte[] rgba = { r, g, b, a };
+			return FromBytes(rgba);
+		}
+
+		public static Color FromBytes(params byte[] rgba)
+		{
+			int count = rgba.Length;
+			float r = count >= 1 ? (rgba[0] / 255f) : 0f;
+			float g = count >= 2 ? (rgba[1] / 255f) : 0f;
+			float b = count >= 3 ? (rgba[2] / 255f) : 0f;
+			float a = count >= 4 ? (rgba[3] / 255f) : 1f;
+
+			return new Color(r, g, b, a);
+		}
 	}
 }
